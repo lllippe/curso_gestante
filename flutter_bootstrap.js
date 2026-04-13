1dbd9dd3ad7c8594478a8eabb5df5efd77f421d9
Felipe Pelissari <felipe.pelissari@gmail.com>
2026-04-13 10:45:26 -0300

    adjust

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1423336848"
+    serviceWorkerVersion: "1288395567"
   }
 });
